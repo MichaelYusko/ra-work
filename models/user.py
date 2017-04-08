0b09d0a42e2aba82fd9a1e9fa733bf28f04789c5
MichaelYusko <MichaelYusko>
2017-04-08 23:16:07 +0300

    add AnonymousUser class

---
+++ models/user.py
@@ -0,0 +1,20 @@
+
+
+class AnonymousUser:
+    id = None
+    ERROR = 'System doesn\'t provide a DB representation for AnonymousUser.'
+
+    def __str__(self):
+        return 'AnonymousUser'
+
+    def is_authenticated(self):
+        return False
+
+    def create_user(self, first_name, last_name, email, role):
+        raise NotImplementedError(self.ERROR)
+
+    def check_password(self, password):
+        return NotImplementedError(self.ERROR)
+
+    def set_password(self, password):
+        raise NotImplementedError(self.ERROR)
