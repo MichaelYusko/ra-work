cf4e0c430fce2a4ea389b841999d45cf71908c96
MichaelYusko <MichaelYusko>
2017-04-10 13:23:20 +0300

    add user table(not complete yet)

--- models/user.py
+++ models/user.py
@@ -1,5 +1,10 @@
 from enum import Enum
 
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+
+from db.compat import utcnow
+from models import Base
+
 
 class UserRole(Enum):
     EMPLOYEE = 1
@@ -24,3 +29,17 @@ class AnonymousUser:
 
     def set_password(self, password):
         raise NotImplementedError(self.ERROR)
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    email = Column(String(254))
+    password = Column(String(128))
+    role = Column(Integer, default=UserRole.EMPLOYEE)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=utcnow())
+    updated_at = Column(DateTime(timezone=True), onupdate=utcnow())
